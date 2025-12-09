fb68b6d1f79ac4e0946b23e0b3aeba58ac1d390a
Delat co <114985494+Delaterr@users.noreply.github.com>
2025-12-09 22:03:41 +0300

    Add files via upload

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2380633974-1a0ad",
+  "appId": "1:864284192960:web:2f63a18a595bf415f3bd25",
+  "apiKey": "AIzaSyAZQ6fzNcHN5dqUqRKAqnmNWbgMQLhIPl4",
+  "authDomain": "studio-2380633974-1a0ad.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "864284192960"
+};
